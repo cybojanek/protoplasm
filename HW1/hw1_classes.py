11defb8c8b20d00145fafb0e9cf705bb1f0cf02d
Jan Kasiak <j.kasiak@gmail.com>
2013-02-01 18:52:22 -0500

    Implement 1,2 of part B and clean up tests

--- HW1/hw1_classes.py
+++ HW1/hw1_classes.py
@@ -1,43 +1,87 @@
-
 class PBF:
-    pass
+    def __init__(self):
+        raise NotImplementedError("PBF class not callable")
+
+    def isNNF(self):
+        raise NotImplementedError("isNNF not implemented")
+
+    def toNNF(self):
+        raise NotImplementedError("toNNF not implemented")
+
 
 class OR(PBF):
-    
     def __init__(self, f1, f2):
+        if not isinstance(f1, PBF) or not isinstance(f2, PBF):
+            raise TypeError("OR can only hold PBF types")
         self.lchild = f1
         self.rchild = f2
 
+    def isNNF(self):
+        return self.lchild.isNNF() and self.rchild.isNNF()
+
+    def toNNF(self):
+        return OR(self.lchild.toNNF(), self.rchild.toNNF())
+
     def __str__(self):
-        return str(self.lchild) + " " + str(self.rchild) + " |"
+        return "%s %s |" % (self.lchild, self.rchild)
 
 
 class AND(PBF):
-
     def __init__(self, f1, f2):
+        if not isinstance(f1, PBF) or not isinstance(f2, PBF):
+            raise TypeError("AND can only hold PBF types")
         self.lchild = f1
         self.rchild = f2
 
+    def isNNF(self):
+        return self.lchild.isNNF() and self.rchild.isNNF()
+
+    def toNNF(self):
+        return AND(self.lchild.toNNF(), self.rchild.toNNF())
+
     def __str__(self):
-        return str(self.lchild) + " " + str(self.rchild) + " &"
+        return "%s %s &" % (self.lchild, self.rchild)
 
 
 class NOT(PBF):
-
     def __init__(self, f):
+        if not isinstance(f, PBF):
+            raise TypeError("NOT can only hold PBF types")
         self.child = f
 
+    def isNNF(self):
+        return isinstance(self.child, PROP)
+
+    def toNNF(self):
+        if self.isNNF():
+            return NOT(self.child.toNNF())
+        elif isinstance(self.child, NOT):
+            return self.child.child.toNNF()
+        else:
+            lchild = self.child.lchild.toNNF()
+            lchild = lchild.child if isinstance(lchild, NOT) else NOT(lchild)
+            rchild = self.child.rchild.toNNF()
+            rchild = rchild.child if isinstance(rchild, NOT) else NOT(rchild)
+            if isinstance(self.child, AND):
+                return OR(lchild, rchild)
+            elif isinstance(self.child, OR):
+                return AND(lchild, rchild)
+
     def __str__(self):
-        return str(self.child) + " !"
+        return "%s !" % (self.child)
 
 
 class PROP(PBF):
-
     def __init__(self, p):
+        if isinstance(p, PBF):
+            raise TypeError("PROP cannot be a PBF")
         self.prop = p
 
+    def isNNF(self):
+        return True
+
+    def toNNF(self):
+        return PROP(self.prop)
+
     def __str__(self):
-        return self.prop
-
-
-    
+        return str(self.prop)
--- HW1/tests.py
+++ HW1/tests.py
@@ -1,4 +1,6 @@
 from hw1_lists import dup, def_use, ssa
+from hw1_classes import PBF, OR, AND, NOT, PROP
+from nose.tools import *
 import itertools
 
 
@@ -6,24 +8,23 @@ def test_dup():
     tests = [
         ([1, 2, 3, 4], False),
         ([1, 2, 3, 2, 4], True),
-        ]
+    ]
     for test in tests:
         query, answer = test
         d = dup(query)
-        print 'Query: %s\nReply :%s\nAnswer:%s' % (query, d, answer)
-        assert dup(query) == answer
+        eq_(d, answer, msg="\nQuery: %s\n%r != %r" % (query, d, answer))
 
 
 def test_def_use():
     tests = [
         ([ ['x', []], ['y', ['x']], ['z', ['x', 'y']], ['x', ['z', 'y']] ], []),
         ([ ['x', []], ['y', ['y', 'x']], ['z', ['x', 'w']], ['x', ['x', 'z']] ], ['y', 'w']),
-        ]
+    ]
     for test in tests:
         query, answer = test
         d = def_use(query)
-        print 'Query: %s\nReply :%s\nAnswer:%s' % (query, d, answer)
-        assert set(def_use(query)) == set(answer)
+        eq_(len(d), len(answer), msg="\nQuery: %s\nlen(%r) != len(%r)" % (query, d, answer))
+        eq_(set(d), set(answer), msg="\nQuery: %s\n%r != %r" % (query, d, answer))
 
 
 def test_ssa():
@@ -32,11 +33,27 @@ def test_ssa():
          [ ['x', []], ['y', ['x']], ['x1', ['x', 'y']], ['z', ['x1', 'y']], ['y1', ['z', 'x1']] ]),
         ([ ['x', []], ['x', ['x']], ['x', ['x']] ],
          [ ['x', []], ['x1', ['x']], ['x2', ['x1']] ]),
-        ]
+    ]
     for test in tests:
         query, answer = test
         d = ssa(query)
-        print 'Query: %s\nReply :%s\nAnswer:%s' % (query, d, answer)
-        assert len(d) == len(answer)
+        eq_(len(d), len(answer), msg="\nQuery: %s\nlen(%r) != len(%r)" % (query, d, answer))
         for x, y in itertools.izip(d, answer):
-            assert x == y
+            eq_(x, y, msg="\nQuery: %s\n%r != %r" % (query, d, answer))
+
+
+def test_NNF():
+    tests = [
+        (PROP('x'), True, 'x'),
+        (NOT(PROP('x')), True, "x !"),
+        (AND(PROP('x'), NOT(PROP('y'))), True, "x y ! &"),
+        (NOT(AND(PROP('x'), NOT(PROP('y')))), False, "x ! y |"),
+        (NOT(NOT(PROP('x'))), False, 'x'),
+        (NOT(NOT(NOT(PROP('x')))), False, "x !")
+    ]
+    for test in tests:
+        query, answer_a, answer_b = test
+        d = query.isNNF()
+        eq_(d, answer_a, msg="\nQuery: %s\n%r != %r" % (query, d, answer_a))
+        d = str(query.toNNF())
+        eq_(str(d), answer_b, msg="\nQuery: %s\n%r != %r" % (query, d, answer_b))
