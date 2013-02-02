5ae696d966e043f3154bcbdaf9fe43ec35a87447
Jan Kasiak <j.kasiak@gmail.com>
2013-02-02 04:16:18 -0500

    Implement part C (efficiently)

---
+++ HW1/hw1_mips.asm
@@ -0,0 +1,35 @@
+.data
+
+.text
+main:
+li $v0, 5			# Read integer n
+syscall
+
+li $a0, -1			# -1 return value if n is <= 0
+blez $v0, exit		# check if n is <= 0, is so then exit
+
+move $t0, $v0		# Move integer n to t0
+li $t1, 0			# Set exponent k to 0
+li $t2, 2			# Set current power to 2^1
+li $t4, 1			# Max
+sll $t4, $t4, 30
+
+while:
+div $t0, $t2		# Integer division of n by (2,4,8,16...)
+mflo $t3
+add $t1, $t1, $t3   # k += n / (2,4,8,16...)
+bge $t2, $t4, end	# Check that our next shift will not overflow
+sll $t2, $t2, 1		# Multiply by 2 for (2,4,8,16...)
+bgt $t2, $t0, end	# If the next power is > n, we're done
+j while				# Go back if (2,4,8,16...) <= n
+
+end:
+li $v0, 1			# Print integer
+move $a0, $t1
+syscall
+
+li $a0, 0			# All is good
+
+exit:
+li $v0, 17			# Exit with value
+syscall
