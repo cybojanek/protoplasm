078edf6b23020fee1d3166c81c14ef0c3f497ceb
Jan Kasiak <j.kasiak@gmail.com>
2013-02-02 19:05:36 -0500

    Clean up mips code by using shifts instead of division

--- HW1/hw1_mips.asm
+++ HW1/hw1_mips.asm
@@ -3,7 +3,7 @@
 # Output: max(k) for 2^k such that n! is divisble by 2^k                     #
 # Algorithm: sum of n/2 + n/4 + n/8 + ... + n/2^31                           #
 # Collect all twos from prime factorization of n, n-1, ..., 1                #
-# Runtime: O(log_2(n))                                                       #
+# Runtime: O(1)                                                              #
 ##############################################################################
 .data
 
@@ -17,17 +17,14 @@ blez $v0, exit		# check if n is <= 0, is so then exit
 
 move $t0, $v0		# Move integer n to t0
 li $t1, 0			# Set exponent k to 0
-li $t2, 2			# Set current power to 2^1
-li $t4, 1			# Max to prevent overflow from next shift
-sll $t4, $t4, 30
+li $t2, 0			# Set shift to 0
 
-while:
-div $t3, $t0, $t2	# Integer division of n by (2,4,8,16...)
-add $t1, $t1, $t3   # k += n / (2,4,8,16...)
-bge $t2, $t4, end	# Check that our next shift will not overflow
-sll $t2, $t2, 1		# Multiply by 2 for (2,4,8,16...)
-bgt $t2, $t0, end	# If the next power is > n, we're done
-j while				# Go back if (2,4,8,16...) <= n
+for:
+sra $t0, $t0, 1		# Divide by 2
+add $t1, $t1, $t0	# Add to k
+addi $t2, $t2, 1	# Count how many times we've shifted
+beq $t2, 31, end	# Stop if we've shifted 31 times
+j for				# Go again
 
 end:
 li $v0, 1			# Print integer
