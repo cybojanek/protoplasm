1c8cc33c53f2d31813957eb8b8bf2b8707af63ed
Jan Kasiak <j.kasiak@gmail.com>
2013-02-10 16:26:44 -0500

    Replace tabs with spaces in asm

--- HW1/hw1_mips.asm
+++ HW1/hw1_mips.asm
@@ -9,30 +9,30 @@
 
 .text
 main:
-li $v0, 5			# Read integer n
+li $v0, 5            # Read integer n
 syscall
 
-li $a0, 1			# -1 return value if n is <= 0
-blez $v0, exit		# check if n is <= 0, is so then exit
+li $a0, 1            # -1 return value if n is <= 0
+blez $v0, exit       # check if n is <= 0, is so then exit
 
-move $t0, $v0		# Move integer n to t0
-li $t1, 0			# Set exponent k to 0
-li $t2, 0			# Set shift to 0
+move $t0, $v0        # Move integer n to t0
+li $t1, 0            # Set exponent k to 0
+li $t2, 0            # Set shift to 0
 
 for:
-sra $t0, $t0, 1		# Divide by 2
-add $t1, $t1, $t0	# Add to k
-addi $t2, $t2, 1	# Count how many times we've shifted
-beq $t2, 31, end	# Stop if we've shifted 31 times
-j for				# Go again
+sra $t0, $t0, 1      # Divide by 2
+add $t1, $t1, $t0    # Add to k
+addi $t2, $t2, 1     # Count how many times we've shifted
+beq $t2, 31, end     # Stop if we've shifted 31 times
+j for                # Go again
 
 end:
-li $v0, 1			# Print integer
+li $v0, 1            # Print integer
 move $a0, $t1
 syscall
 
-li $a0, 0			# All is good
+li $a0, 0            # All is good
 
 exit:
-li $v0, 17			# Exit with value
+li $v0, 17           # Exit with value
 syscall
