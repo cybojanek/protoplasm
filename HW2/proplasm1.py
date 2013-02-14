91d1a6e2c153a9af5ffe30e2c13a94b4e260e831
Jan Kasiak <j.kasiak@gmail.com>
2013-02-13 21:01:39 -0500

    Implement parser for CFG of Assignment 2
    * Add some simple test for TokenError, SyntaxError and no errors

--- HW2/proplasm1.py
+++ HW2/proplasm1.py
@@ -15,28 +15,213 @@ import sys
 import tokenize
 
 
-def proto_tokenize(file_name):
-    statements = []
-    statement = []
-    g = tokenize.generate_tokens(open(file_name, 'r').readline)
+def proto_tokenize(file_name=None, lines=None):
+    print lines
+    tokens = []
+    if file_name is not None:
+        g = tokenize.generate_tokens(open(file_name, 'r').readline)
+    elif lines is not None:
+        it = iter(lines).next
+        g = tokenize.generate_tokens(it)
     for toknum, tokval, start, end, line in g:
         # Don't add newlines: we're looking for ;
-        if toknum == tokenize.NEWLINE:
+        if toknum in (tokenize.NL, tokenize.NEWLINE):
             continue
-        statement.append([toknum, tokval, start, end, line])
-        # If we hit a ; then its an end of statement
-        if toknum == tokenize.OP and tokval == ';':
-            statements.append(statement)
-            statement = []
-    for x in statements:
-        print '%r' % x[0][4]
-        for t in x:
-            tokenize.printtoken(*t)
-    return statements
+        tokens.append([toknum, tokval, start, end, line])
+    for x in tokens:
+        tokenize.printtoken(*x)
+    return tokens
+
+
+def Raise(s, x):
+    toknum, tokval, start, end, line = x[0]
+    raise SyntaxError('%s near line %s, column %s: %r' %
+        (s, start[0], start[1], line))
+
+
+def empty(x):
+    return len(x) == 0
+
+
+class ProtoSyntaxChecker(object):
+
+    SUM_OP = set(['+', '-'])
+    PROD_OP = set(['*', '/', '%'])
+
+    def __init__(self, tokens):
+        """Create a syntax checker for a list of tokens.
+        Implements CFG from Homework 2
+
+        Arguments:
+        tokens - list of tuples (toknum, tokval, start, end, line)
+
+        """
+        self.tokens = tokens
+
+    def check(self):
+        """Start parser and check for syntax errors.
+        Raise SyntaxError on bad syntax.
+
+        """
+        self.parse_PRGM(self.tokens)
+
+    def parse_PRGM(self, x):
+        """
+        Pgm -> Stmt Pgm
+            | Stmt
+        """
+        rest = self.parse_STMT(x)
+        if empty(rest):
+            return True
+        else:
+            return self.parse_PRGM(rest)
+
+    def parse_STMT(self, x):
+        """
+        Stmt -> Assign
+             | Print
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        # Peek at print
+        if toknum == tokenize.NAME and tokval == 'print':
+            return self.parse_PRINT(x)
+        # Peek at variable
+        elif toknum == tokenize.NAME:
+            return self.parse_ASSIGN(x)
+        # Peek at end of tokens
+        elif toknum == tokenize.ENDMARKER:
+            return []
+        else:
+            Raise('Incorrent STMT!', x)
+
+    def parse_PRINT(self, x):
+        """
+        Print -> 'print' '(' AE ')' ';'
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if toknum == tokenize.NAME and tokval == 'print':
+            return self.consume_SEMICOLON(self.consume_RPAREN(self.parse_AE(
+                self.consume_LPAREN(tail))))
+        else:
+            Raise('Expected PRINT', x)
+
+    def parse_ASSIGN(self, x):
+        """
+        Assign -> var '=' Rhs ';'
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if toknum == tokenize.NAME:
+            return self.consume_SEMICOLON(self.parse_RHS(
+                self.consume_EQUAL(tail)))
+        else:
+            Raise('Expected NAME', x)
+
+    def parse_RHS(self, x):
+        """
+        Rhs -> 'input' '(' ')'
+            | AE
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if toknum == tokenize.NAME and tokval == 'input':
+            return self.consume_RPAREN(self.consume_LPAREN(tail))
+        else:
+            return self.parse_AE(x)
+
+    def parse_AE(self, x):
+        """
+        AE -> T SumOp AE
+           | T
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        rest = self.parse_T(x)
+        if not empty(rest) and rest[0][1] in ProtoSyntaxChecker.SUM_OP:
+            return self.parse_AE(rest[1:])
+        else:
+            return rest
+
+    def parse_T(self, x):
+        """
+        T -> F ProdOp T
+          | F
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        rest = self.parse_F(x)
+        if not empty(rest) and rest[0][1] in ProtoSyntaxChecker.PROD_OP:
+            return self.parse_T(rest[1:])
+        else:
+            return rest
+
+    def parse_F(self, x):
+        """
+        F -> '-' F
+          | '(' AE ')'
+          | intconst
+          | var
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if toknum == tokenize.MINUS:
+            return self.parse_F(tail)
+        elif toknum == tokenize.OP and tokval == '(':
+            return self.consume_RPAREN(self.parse_AE(tail))
+        elif toknum == tokenize.NAME:
+            return tail
+        elif toknum == tokenize.NUMBER and tokval.isdigit():
+            return tail
+        else:
+            Raise('Bad statement!', x)
+
+    def consume_LPAREN(self, x):
+        """Consume a LPAREN from head and return tail
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if not empty(x) and toknum == tokenize.OP and tokval == '(':
+            return tail
+        else:
+            Raise('Missing LPAREN', x)
+
+    def consume_RPAREN(self, x):
+        """Consume a RPAREN from head and return tail
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if not empty(x) and toknum == tokenize.OP and tokval == ')':
+            return tail
+        else:
+            Raise('Missing RPAREN', x)
+
+    def consume_EQUAL(self, x):
+        """Consume an EQUAL from head and return tail
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if not empty(x) and toknum == tokenize.OP and tokval == '=':
+            return tail
+        else:
+            Raise('Missing EQUAL sign', x)
+
+    def consume_SEMICOLON(self, x):
+        """Consume a SEMICOLON from head and return tail
+        """
+        head, tail = x[0], x[1:]
+        toknum, tokval, _, _, _ = head
+        if not empty(x) and toknum == tokenize.OP and tokval == ';':
+            return tail
+        else:
+            Raise('Missing SEMICOLON sign', x)
 
 
 def main(file_name):
     statement_tokens = proto_tokenize(file_name)
+    psc = ProtoSyntaxChecker(statement_tokens)
+    psc.check()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
---
+++ HW2/tests.py
@@ -0,0 +1,59 @@
+from proplasm1 import proto_tokenize, ProtoSyntaxChecker
+
+from nose.tools import *
+from tokenize import TokenError
+
+
+def add_newlines(array):
+    return ['%s\n' % x for x in array]
+
+
+def test_proto_token_errors():
+    """proto1 token errors"""
+
+    @raises(TokenError)
+    def tokenize_with_error(lines):
+        proto_tokenize(lines=lines)
+
+    tests = [
+        # Expects continuation of multi-line statement
+        ['x = 2 * (2 -1'],
+    ]
+    for test in tests:
+        tokenize_with_error(add_newlines(test))
+
+
+def test_proto_syntax_errors():
+    """proto1 syntax errors"""
+
+    @raises(SyntaxError)
+    def parse_with_error(lines):
+        tokens = proto_tokenize(lines=lines)
+        psc = ProtoSyntaxChecker(tokens)
+        psc.check()
+
+    tests = [
+        # Missing semicolon
+        ['x = 1'],
+        # Trying to assign to reserved word print
+        ['x = 1;', 'print = x;'],
+        # Missing operand
+        ['x = 1;', 'y = x + 2 *;'],
+    ]
+    for test in tests:
+        parse_with_error(add_newlines(test))
+
+
+def test_proto_syntax():
+    """proto1 syntax"""
+
+    tests = [
+        # Base case
+        ['x = 1; print(x);'],
+        # Multi line paranthesis should work
+        ['x = 2 * (2 -', '1 + 2);'],
+    ]
+    for test in tests:
+        tokens = proto_tokenize(lines=add_newlines(test))
+        psc = ProtoSyntaxChecker(tokens)
+        psc.check()
