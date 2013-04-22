144b937672ff3911f7adaaf738ee22c91b2019c9
Jan Kasiak <j.kasiak@gmail.com>
2013-04-22 00:16:34 -0400

    Fix up syntax highlighting and add fibonnacci test
    * Change int main to void main with return in tests

--- HW5/tests/tests.py
+++ HW5/tests/tests.py
@@ -75,6 +75,8 @@ def test_compile_errors():
 def test_recursion():
     """recursion"""
     compile_and_run('factorial', [120, 479001600])
+    compile_and_run('fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+                                  233, 377, 610, 987, 1597, 2584, 4181])
 
 
 @pre_entry
